793c3a4f6d80cec8d3f11dc26b1af45b873e72c4
snorlax <68203941+snorlax93@users.noreply.github.com>
2021-12-24 01:51:21 -0500

    Update App.test.js
    dummy tests for pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('dummy test for pipeline pass', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Accounts/i);
   expect(linkElement).toBeInTheDocument();
 });
